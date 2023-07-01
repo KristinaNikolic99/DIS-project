4e9ec106fb05474560d02a4c513f4c29c00b82dd
KristinaNikolic99 <krisnikolic992@gmail.com>
2023-07-01 22:25:37 +0200

    Added API Gateway microservice

---
+++ API-Gateway/src/main/java/APIGateway/APIGatewayConfiguration.java
@@ -0,0 +1,19 @@
+package APIGateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class APIGatewayConfiguration {
+
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes().route(p -> p.path("/get").uri("http://httpbin.org:80")).
+				route(p -> p.path("/api/company/**").uri("lb://company-service"))
+				.route(p -> p.path("/api/sector/**").uri("lb://sector-service"))
+				.route(p -> p.path("/api/education/**").uri("lb://education-service"))
+				.build();
+	}
+}
