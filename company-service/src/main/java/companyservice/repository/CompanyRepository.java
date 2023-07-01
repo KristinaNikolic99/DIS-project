a861b1801fd8a5184daac7651418da26f6a31787
KristinaNikolic99 <krisnikolic992@gmail.com>
2023-07-01 22:22:53 +0200

    Completed changes in application.properties

--- company-service/src/main/java/companyservice/repository/CompanyRepository.java
+++ company-service/src/main/java/companyservice/repository/CompanyRepository.java
@@ -2,7 +2,6 @@ package companyservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import companyservice.model.Company;
 
 @Repository
--- company-service/src/main/java/companyservice/service/CompanyService.java
+++ company-service/src/main/java/companyservice/service/CompanyService.java
@@ -1,10 +1,8 @@
 package companyservice.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import companyservice.model.Company;
 import companyservice.repository.CompanyRepository;
 
